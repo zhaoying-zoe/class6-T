bac19a88f11bc367853638a45e7e97ffeaa98085
赵颖 <1130051785@qq.com>
2019-11-15 20:54:43 +0800

    2019/11/15

---
+++ 01-chuji/03-js/js/007.js
@@ -0,0 +1,14 @@
+//函数//
+function toBig(){
+	var obox=document.getElementById('box')
+	obox.style.background='blue';
+	obox.style.width='400px';
+	obox.style.height='400px';
+}
+function toSmall(){
+	var obox=document.getElementById('box')
+	obox.style.background='tomato';
+	obox.style.width='200px';
+	obox.style.height='200px';
+}
+document.getElementById('box2').style.color='blue'
