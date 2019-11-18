0a79bc81c6c46c0625c737832a624552f5dc96fd
赵颖 <1130051785@qq.com>
2019-11-18 12:06:19 +0800

    2019

--- practice/js/index.js
+++ practice/js/index.js
@@ -292,6 +292,3 @@ pauseBtn.onclick = function() {
     game.start();
     pauseBtn.parentNode.style.display='none';
 }
-————————————————
-版权声明：本文为CSDN博主「zoyoy」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/weixin_40693643/article/details/103048198
