8e3900c3705981c0f4df6b86aba4f9e1e1555b1a
“zhaoying-zoe < https://github.com/zhaoying-zoe>
2020-06-07 22:10:47 +0800

    2020/6/7

--- 02-zhongji/03-project/js/index.js
+++ 02-zhongji/03-project/js/index.js
@@ -1,16 +1,13 @@
 ;(function($){
-	/*顶部导航逻辑------开始*/
-		$('.top .dropdown').hover(function(){
-			/*
-				console.log(this);// this指.dropdown
-				$(this).addClass('menu-active');// 添加类名 menu-active
-			 */
-			var $this = $(this);
-			var active = $this.data('menu');
-			console.log($this.data());
-			$(this).addClass(active +'-active');
-		},function(){
-			$(this).removeClass('menu-active');// 移除类名 menu-active
-		})
-	/*顶部导航逻辑------结束*/
+/*顶部导航逻辑------开始*/
+	var $dropdown = $('.top .dropdown');
+	$dropdown.dropdown({
+		js:true,
+		mode:'slideDownUp'
+	});
+	//监听显示隐藏事件
+	$dropdown.on('dropdown-show dropdown-shown dropdown-hide dropdown-hidden',function(ev){
+		console.log(ev.type);
+	})
+/*顶部导航逻辑------结束*/
 })(jQuery);
