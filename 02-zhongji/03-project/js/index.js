0b388504881c0517f85f548804bde9a6d7fa5d6b
“zhaoying-zoe < https://github.com/zhaoying-zoe>
2020-06-10 19:51:42 +0800

    2020/6/10

--- 02-zhongji/03-project/js/index.js
+++ 02-zhongji/03-project/js/index.js
@@ -8,6 +8,19 @@
 	//监听显示隐藏事件
 	$dropdown.on('dropdown-show dropdown-shown dropdown-hide dropdown-hidden',function(ev){
 		console.log(ev.type);
+		if(ev.type == 'dropdown-show'){
+			var $this = $(this);
+			var dropdownLayer = $this.find('.dropdown-layer');
+			var url = $this.data('load');
+			$.getJSON(url,function(){
+				var html = '';
+				// for(var i = 0;i<data.length;i++){
+				// 	html += "<li><a href=" '+ data[i].url +' ">+'data[i].name'+</a></li>"
+				// }
+			});
+		}
 	})
+	// 懒加载
+
 /*顶部导航逻辑------结束*/
 })(jQuery);
