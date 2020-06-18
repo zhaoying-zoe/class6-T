859266cec32aa954d05b278089a304e6fde7a71a
“zhaoying-zoe < https://github.com/zhaoying-zoe>
2020-06-18 23:02:05 +0800

    2020.6.18

--- 02-zhongji/04-html-advanced/001-server.js
+++ 02-zhongji/04-html-advanced/001-server.js
@@ -5,9 +5,10 @@ var fs = require('fs');// 核心模块 专门用来读文件
 var server = http.createServer(function(req,res){
 	// console.log(req.headers.cookie);
 	// res.setHeader('Set-Cookie','name=tom');// 创建Cookie
-	var oDate = new Date('2020-6-18 11:13:00').toUTCString()
-	res.setHeader('Set-Cookie','name=tom;Expires='+oDate);
+	// var oDate = new Date('2020-6-18 11:13:00').toUTCString()
+	// res.setHeader('Set-Cookie','name=tom;Expires='+oDate);
 	// res.setHeader('Set-Cookie','name=tom;MAX-Age='+10);
+	
 	if(req.url == './favicon.ico'){
 		res.end('ok')
 	}
