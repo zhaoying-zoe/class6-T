717557ba5f07c0cff3bf8fe200d44f9b9922ef91
赵颖 <1130051785@qq.com>
2020-07-03 18:00:54 +0800

    2020.7.3

---
+++ 03-gaoji/02-nodejs/05-stream/002-custom-writeable.js
@@ -0,0 +1,8 @@
+const {Writeable} = require('stream');// 对象的结构解析
+
+// 继承
+class writer extends Writeable{
+	_write(){}
+	
+};
+
---
+++ 03-gaoji/02-nodejs/06-fs/001-fs-read.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// 同步读写文件
+
+/*
+// 1.读取文件 fs.openSync(path[, flags, mode])
+const fd = fs.openSync('./001.txt','a');
+// 2.写入并保存 fs.writeSync(fd, string[, position[, encoding]])
+fs.writeSync(fd,'hello 办单');
+// 3.关闭文件 fs.closeSync(fd)
+fs.closeSync(fd);
+*/
+
+
+// 一步写成
+// fs.writeFileSync(file, data[, options])
+fs.writeFileSync('./001.txt','hello',{flag:'a'});
