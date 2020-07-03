88a921f40fe2df71f2701a4fd61840d8475b07e1
赵颖 <1130051785@qq.com>
2020-07-03 10:12:27 +0800

    3

--- 03-gaoji/02-nodejs/03-global/pkd/003-timer.js
+++ 03-gaoji/02-nodejs/03-global/pkd/003-timer.js
@@ -3,3 +3,21 @@ setTimeout(()=>{
 },200);
 
 console.log('global 222');
+
+clearTimeout();
+
+
+
+setInterval(()=>{
+	console.log('timer t2');
+},200)
+clearInterval();
+
+setImmediate(()=>{
+	console.log('timer t3');
+})
+clearImmediate();
+
+process.nextTick(()=>{
+	console.log('timer t4');
+})
