cf14117d8659867129103ad0f2fe8e0286c2eddd
“zhaoying-zoe <57398099+zhaoying-zoe@users.noreply.github.com>
2020-08-30 14:01:59 +0800

    结构样式

--- 03-gaoji/10-wechat/pages/article/article.js
+++ 03-gaoji/10-wechat/pages/article/article.js
@@ -6,14 +6,51 @@ Page({
      */
     data: {
         name:'tom',
+        arr:[
+            {
+                name:'zhuzhu',
+            },
+            {
+                name:'xiaozhuzhu',
+            }
+        ],
+        /* 文章列表数据 */
+        articles:[
+            {
+                headPortrait:'/pages/images/avatar/u1.jpg',
+                author:'小猪',
+                time:'2天前',
+                avatar:'/pages/images/avatar/u1.jpg',
+                title:'我是文章标题',
+                mainImage:'/pages/images/article/a1.jpg',
+                desc:'我是文章1描述',
+                star:20,
+                view:'56'
+            },
+            {
+                headPortrait:'/pages/images/avatar/u2.jpg',
+                author:'小明',
+                time:'3天前',
+                avatar:'/pages/images/avatar/u2.jpg',
+                title:'我是文章标题',
+                mainImage:'/pages/images/article/a2.jpg',
+                desc:'我是文章2描述',
+                star:20,
+                view:'56'
+            },
+            {
+                headPortrait:'/pages/images/avatar/u3.jpg',
+                author:'小王',
+                time:'4天前',
+                avatar:'/pages/images/avatar/u3.jpg',
+                title:'我是文章标题',
+                mainImage:'/pages/images/article/a3.jpg',
+                desc:'我是文章3描述',
+                star:101,
+                view:'250'
+            },
+        ]
     },
-    changeName: function(e) {
-        // sent data change to view
-        this.setData({
-          name: 'love-zhuzhu'
-        })
-    },
-
     /**
      * 生命周期函数--监听页面加载
      */
@@ -68,5 +105,34 @@ Page({
      */
     onShareAppMessage: function () {
 
+    },
+    onShareAppMessage() {
+        return {
+          title: 'swiper',
+          path: 'page/component/pages/swiper/swiper'
+        }
+    },
+    changeIndicatorDots() {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    },
+
+    changeAutoplay() {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+
+    intervalChange(e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+
+    durationChange(e) {
+        this.setData({
+            duration: e.detail.value
+        })
     }
 })
