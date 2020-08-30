7d7760cf6bf29edd7d8bfae9d7e36d0049dd8f33
“zhaoying-zoe <57398099+zhaoying-zoe@users.noreply.github.com>
2020-08-30 16:42:42 +0800

    wechat The life cycle

--- 03-gaoji/10-wechat/pages/article/article.js
+++ 03-gaoji/10-wechat/pages/article/article.js
@@ -1,6 +1,5 @@
 // pages/article/article.js
 Page({
-
     /**
      * 页面的初始数据
      */
@@ -55,56 +54,63 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        console.log('article onLoad ...');
     },
-
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        console.log('article onReady ...');
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        console.log('article onShow ...');
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-
+        console.log('article onHide ...');
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
+        console.log('article onUnload ...');
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+        console.log('article onPullDownRefresh ...');
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        console.log('article onReachBottom ...');
     },
 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-
+        console.log('article onShareAppMessage ...');
+    },
+    onPageScroll: function() {
+        // 页面滚动时执行
+        console.log('article onPageScroll ...');
+    },
+    onResize: function() {
+        // 页面尺寸变化时执行
+        console.log('article onResize ...');
     },
     onShareAppMessage() {
         return {
