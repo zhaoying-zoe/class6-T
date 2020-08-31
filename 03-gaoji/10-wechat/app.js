4cfce1c46053190f692463f5fec6c77444035aef
zhaoying-zoe <57398099+zhaoying-zoe@users.noreply.github.com>
2020-08-31 11:54:35 +0800

    article-detail

--- 03-gaoji/10-wechat/app.js
+++ 03-gaoji/10-wechat/app.js
@@ -2,15 +2,15 @@
 App({
   onLaunch (options) {
     // Do something initial when launch.  加载时
-    console.log('app onLaunch ...')
+    // console.log('app onLaunch ...')
   },
   onShow (options) {
     // Do something when show.  展示时
-    console.log('app onShow ...')
+    // console.log('app onShow ...')
   },
   onHide () {
     // Do something when hide.  隐藏时
-    console.log('app onHide ...')
+    // console.log('app onHide ...')
   },
   onError (msg) {
     // 错误时
--- 03-gaoji/10-wechat/pages/article/article.js
+++ 03-gaoji/10-wechat/pages/article/article.js
@@ -1,4 +1,5 @@
 // pages/article/article.js
+var { articles } = require('../../db/index.js');
 Page({
     /**
      * 页面的初始数据
@@ -14,103 +15,78 @@ Page({
             }
         ],
         /* 文章列表数据 */
-        articles:[
-            {
-                headPortrait:'/pages/images/avatar/u1.jpg',
-                author:'小猪',
-                time:'2天前',
-                avatar:'/pages/images/avatar/u1.jpg',
-                title:'我是文章标题',
-                mainImage:'/pages/images/article/a1.jpg',
-                desc:'我是文章1描述',
-                star:20,
-                view:'56'
-            },
-            {
-                headPortrait:'/pages/images/avatar/u2.jpg',
-                author:'小明',
-                time:'3天前',
-                avatar:'/pages/images/avatar/u2.jpg',
-                title:'我是文章标题',
-                mainImage:'/pages/images/article/a2.jpg',
-                desc:'我是文章2描述',
-                star:20,
-                view:'56'
-            },
-            {
-                headPortrait:'/pages/images/avatar/u3.jpg',
-                author:'小王',
-                time:'4天前',
-                avatar:'/pages/images/avatar/u3.jpg',
-                title:'我是文章标题',
-                mainImage:'/pages/images/article/a3.jpg',
-                desc:'我是文章3描述',
-                star:101,
-                view:'250'
-            },
-        ]
+        articles:[]
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log('article onLoad ...');
+        // 页面加载完毕后获取数据
+        // console.log(this.data);
+        // this.data.articles = articles;// 错误的写法
+        this.setData({articles:articles},function(){
+            // 异步
+            // console.log('异步');
+        })
+        // 同步
+        // console.log('同步');
+        
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-        console.log('article onReady ...');
+        // console.log('article onReady ...');
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-        console.log('article onShow ...');
+        // console.log('article onShow ...');
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-        console.log('article onHide ...');
+        // console.log('article onHide ...');
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-        console.log('article onUnload ...');
+        // console.log('article onUnload ...');
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-        console.log('article onPullDownRefresh ...');
+        // console.log('article onPullDownRefresh ...');
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-        console.log('article onReachBottom ...');
+        // console.log('article onReachBottom ...');
     },
 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-        console.log('article onShareAppMessage ...');
+        // console.log('article onShareAppMessage ...');
     },
     onPageScroll: function() {
         // 页面滚动时执行
-        console.log('article onPageScroll ...');
+        // console.log('article onPageScroll ...');
     },
     onResize: function() {
         // 页面尺寸变化时执行
-        console.log('article onResize ...');
+        // console.log('article onResize ...');
     },
     onShareAppMessage() {
         return {
@@ -139,6 +115,15 @@ Page({
     durationChange(e) {
         this.setData({
             duration: e.detail.value
+        })
+    },
+    topArticle:function(ev){
+        var articleId = ev.currentTarget.dataset.articleId
+        // console.log(ev.currentTarget.dataset);
+        // console.log(articleId);
+        // console.log(wx);
+        wx.navigateTo({
+            url:'/pages/article/article-detail/article-detail?articleId=' + articleId,
         })
     }
 })
